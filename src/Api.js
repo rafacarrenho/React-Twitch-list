2209c98c69a002775cc77e1f81fa112e3803db74
rafacarrenho <rafael.carrenho@hotmail.com>
2020-10-23 08:41:06 -0300

    first commit

---
+++ src/Api.js
@@ -0,0 +1,27 @@
+export const API_URL = "https://api.twitch.tv/helix/streams";
+
+const clienteId = "dvic53afkoytz2erviffk93o1q6c89";
+const clientSecret = "pnqnwz3r4rzbfpgv0ygxnob934lcgl";
+const grantType = "client_credentials";
+
+export function TOKEN_POST() {
+  return {
+    url: `https://id.twitch.tv/oauth2/token?client_id=${clienteId}&client_secret=${clientSecret}&grant_type=${grantType}`,
+    options: {
+      method: "POST",
+    },
+  };
+}
+
+export function GET_STREAMS(token) {
+  return {
+    url: `https://api.twitch.tv/helix/streams`,
+    options: {
+      method: "GET",
+      headers: {
+        "Client-ID": clienteId,
+        Authorization: "Bearer " + token,
+      },
+    },
+  };
+}
--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { GET_STREAMS, TOKEN_POST } from "./Api";
 
 function App() {
+  const [token, setToken] = React.useState(null);
+  const [streams, setStreams] = React.useState(null);
+  const [error, setError] = React.useState(null);
+  const width = 400;
+  const height = 400;
+
+  React.useEffect(() => {
+    async function getToken() {
+      const { url, options } = TOKEN_POST();
+      const response = await fetch(url, options);
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+    getToken();
+  }, []);
+
+  React.useEffect(() => {
+    async function getSteams() {
+      try {
+        setError(null);
+        const { url, options } = GET_STREAMS(token);
+        const response = await fetch(url, options);
+        const json = await response.json();
+        if (json.data) setStreams(json);
+        console.log(streams);
+      } catch (err) {
+        setError(err.message);
+        console.log(error);
+      } finally {
+      }
+    }
+    getSteams();
+  }, [token]);
+
+  React.useEffect(() => {
+    console.log(streams);
+  }, [streams]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {streams &&
+          streams.data.map((itens) => (
+            <div>
+              <figure>
+                <img src={itens.thumbnail_url} alt="" />
+              </figure>
+              <h2>{itens.user_name}</h2>
+              <h3>{itens.title}</h3>
+              <p>{itens.viewer_count}</p>
+            </div>
+          ))}
+        <div>{error && <h1>{error}</h1>}</div>
       </header>
     </div>
   );
